8f43af1f0bf3191746bb8776036485dff95dddc6
Miu Kobayashi <miukoba@gmail.com>
2021-07-05 11:46:48 +0900

    url parsing

---
+++ 55_url-parsing/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+)
+
+func main() {
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("---Scheme---")
+	fmt.Println(u.Scheme)
+
+	fmt.Println("---User---")
+	fmt.Println(u.User)
+	fmt.Println(u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println("---Host---")
+	fmt.Println(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host)
+	fmt.Println(port)
+
+	fmt.Println("---Path---")
+	fmt.Println(u.Path)
+	fmt.Println("---Fragment---")
+	fmt.Println(u.Fragment)
+
+	fmt.Println("---query---")
+	fmt.Println(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println(m)
+	fmt.Println(m["k"][0])
+}
